0bb2e3c50f2a36ea4a018fb95d34a1556fcda834
Mvisruth <visruthm123@gmail.com>
2024-07-14 15:28:36 +0530

    create render-prediction

--- utilites/render-prediction.js
+++ utilites/render-prediction.js
@@ -1,5 +1,24 @@
 export const renderPrediction = (prediction,ctx)=>{
     ctx.clearReact(0,0,ctz.canvas.width,ctz.canvas.height)
     //fonts
-    const 
+    const font = "16px sans-serif"
+    ctx.font = font
+    ctx.textBaseline = "top"
+    
+    
+    prediction.forEach((prediction)=>{
+        const [x,y,width,height]=prediction['box']
+        const isPerson = prediction.class === "person"
+
+        //bounding Box
+        ctx.strokeStyl= isPerson?"#FF0000":"#00FFF"
+        ctx.lineWidth=4;
+        ctx.strokeStyl(x,y,width,height)
+
+        //fill the color
+        ctz.fillstyle = `rgba(255,0,0,${isPerson?0.2:0})`
+        ctx.filRect(x,y,width,height)
+
+        
+    })
 }
